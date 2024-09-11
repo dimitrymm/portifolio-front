a853c0ca7462ff2c34a55db1f5fbf43628934cf2
Dimitry <dimitrymmarinho@gmail.com>
2024-09-10 21:30:05 -0300

    Criação Layout Base

---
+++ src/components/Main.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+
+export default function Main() {
+  return (
+    <>
+      <main className="w-full">
+        <div className="container">
+          <h1>Conheça o Dimitry</h1>
+          <p>Desenvolvedor Front-End</p>
+        </div>
+        <div>
+          <p>
+            Estou aprendendo a desenvolver os melhores produtos utilizando as
+            ferramentas que tragam as melhores experiencias para o usuário, e
+            consequentemente me tornando o melhor programador e crescendo na
+            carreira tech
+          </p>
+        </div>
+        <div>
+          <span>Github</span>
+          <span>LinkedIn</span>
+          <span>BluSky</span>
+        </div>
+        <section>
+          <table>
+            <tbody>
+              <tr>
+                <td>Linguagens:</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Bibliotecas/Frameworks: </td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Banco de Dados:</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Ferramentas:</td>
+                <td></td>
+              </tr>
+            </tbody>
+          </table>
+        </section>
+        <article>
+          <div>
+            <h1>Card</h1>
+          </div>
+        </article>
+        <footer>
+          <div>
+            <h1>Footer</h1>
+          </div>
+        </footer>
+      </main>
+    </>
+  );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+import Header from "../components/Header";
+import Main from "@/components/Main";
+
+export default function Page() {
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  );
+}
